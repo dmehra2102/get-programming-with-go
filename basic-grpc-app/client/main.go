8ea890573d5bcfa1b73ce002069bc9d3a9fcf318
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-07-27 21:33:17 +0530

    feat: added basic Unary rpc

---
+++ basic-grpc-app/client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "grpc-app/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err!= nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+
+	ctx,cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	resp,err := c.SayHello(ctx, &pb.HelloRequest{Name: "Deepanshu Mehra"})
+	if err!= nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", resp.GetMessage())
+}
---
+++ basic-grpc-app/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	pb "grpc-app/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error){
+	log.Printf("Received : %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()},nil
+}
+
+func main() {
+	lis,err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v",err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Println("Starting gRPC listener on port : 50051")
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v",err)
+	}
+}
